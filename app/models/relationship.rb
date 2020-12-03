7df5b782b70690f4f7baec587831bf7fd82360cd
Yasuaki <yasu5293koh@gmail.com>
2020-12-03 04:02:23 +0000

    フォロー機能：モデルにリレーション追加、フォロー機能のメソッド追加

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,11 @@
 class Relationship < ApplicationRecord
+  # class_nameでUserモデルを参照するように設定することでUserをfollowerとfollowedに分ける(関連先のモデルを参照する際の名前を変更)
+  # フォローする人とRelationshipを関連付け(1:N)
+  belongs_to :follower, class_name: "User"
+  # フォローされる人とRelationshipを関連付け(1:N)
+  belongs_to :followed, class_name: "User"
+  
+  # どちらか一つでも無かった場合保存されない
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
