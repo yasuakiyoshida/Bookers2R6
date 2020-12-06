0bf5bea9181d5b379cddd48586ae994efde84d96
Yasuaki <yasu5293koh@gmail.com>
2020-12-06 04:18:19 +0000

    いいね機能：非同期通信に変更

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,7 @@
 class Favorite < ApplicationRecord
 	belongs_to :user
 	belongs_to :book
+	# validates_uniqueness_of:同一のuser_idに対するbook_idは一意である、という制約
+	# scope:で一意性制約を決めるために使用する他のカラムを設定
 	validates_uniqueness_of :book_id, scope: :user_id
 end
