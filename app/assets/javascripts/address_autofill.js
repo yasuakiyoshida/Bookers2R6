04c265ee0695b1885200f432188ec78687267341
Yasuaki <yasu5293koh@gmail.com>
2020-12-08 08:18:26 +0000

    [Fix]ページ遷移して戻ってきてもjqueryが動くように修正

--- app/assets/javascripts/address_autofill.js
+++ app/assets/javascripts/address_autofill.js
@@ -1,4 +1,6 @@
 $(function() {
+  // ページ遷移して該当ページに戻ってくるとjqueryが動かないので、初回読み込み、リロード、ページ切り替えで動くように設定
+  $(document).on('turbolinks:load', () => {
   // .jpostalを指定するセレクタを選択(デベロッパーツールで確認するとid="user_postcode")
   return $('#user_postcode').jpostal({
     // 郵便番号の入力欄が１つの場合の指定
@@ -8,6 +10,7 @@ $(function() {
       '#user_prefecture_code': '%3', // 都道府県が入力される
       '#user_address_city': '%4', // 市が入力される
       '#user_address_street': '%5', // 町村が入力される
-    },
+      }
+    });
   });
 });
