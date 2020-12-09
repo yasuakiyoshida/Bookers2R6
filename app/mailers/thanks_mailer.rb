febd95358bb722437e6b489107b4c2e292595ec6
Yasuaki <yasu5293koh@gmail.com>
2020-12-09 04:49:13 +0000

    メール送信機能：メール作成用メソッド定義、メール内容のビュー作成

--- app/mailers/thanks_mailer.rb
+++ app/mailers/thanks_mailer.rb
@@ -1,2 +1,12 @@
 class ThanksMailer < ApplicationMailer
+  
+  def send_confirm_to_user(user, thanks)
+    # ユーザー情報
+    @user = user
+    # 返信内容
+    @answer = thanks.reply_text
+    # mail to:(宛先) user.email => send_confirm_to_userメソッドを呼び出す時に渡されるユーザーの情報から、emailアドレスだけを取り出してメールの送信先とする
+    #      subject:(件名) => メールのタイトル
+    mail to: user.email, subject: '【TEST】 登録完了'
+  end
 end
